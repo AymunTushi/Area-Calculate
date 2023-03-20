34ba8437f80cfa8d73bbd17fdd5d62a4084670c9
Aymun Tushi <tushiaymun@gmail.com>
2023-03-20 21:58:30 +0600

    rectangle area calculate

--- js/card.js
+++ js/card.js
@@ -1,21 +1,15 @@
-function areaCalculate(){
-    const breadthElement=document.getElementById("breadth")
-const breadthInput=breadthElement.value
-const heightElement=document.getElementById("height")
-const heightInput=heightElement.value
-}
+
 
 let serial=0
 document.getElementById("triangle-btn").addEventListener("click",function(e){
     serial=serial+1
 const triangleBreadthElement=document.getElementById("breadth")
 const triangleBreadthInput=triangleBreadthElement.value
-//const breadthInput=parseInt(triangleBreadthInput)
 const triangleHeightElement=document.getElementById("height")
 const triangleHeightInput=triangleHeightElement.value
 const triangleAreaCalculate=.5*parseFloat(triangleBreadthInput)*parseFloat(triangleHeightInput)
 const triangleFormulaElement=document.getElementById("triangle-formula")
-const triangleArea=parseFloat(triangleFormulaElement.innerText) 
+const triangleArea=(parseFloat(triangleFormulaElement.innerText)).toFixed(2)
 triangleFormulaElement.innerText=triangleAreaCalculate
 
 const contentTitle=document.getElementById("triangle").innerText
@@ -23,18 +17,35 @@ const contentTitle=document.getElementById("triangle").innerText
 const textAreaElement=document.getElementById("container")
 const tr=document.createElement("tr");
 tr.innerHTML=`
-<td>${serial}</td>
+<td>${serial}.</td>
 <td>${contentTitle}</td>
-<td>${triangleAreaCalculate}</td>
+<td>${triangleAreaCalculate}cm2</td>
 
 `
 textAreaElement.appendChild(tr)
 
 
+})
 
+document.getElementById("rectangle-btn").addEventListener("click",function(e){
+    serial=serial+1
+    const rectangleWidthElement=document.getElementById("width")
+    const rectangleWidthInput=rectangleWidthElement.value
+    const rectangleLengthElement=document.getElementById("length")
+    const rectangleLengthInput=rectangleLengthElement.value
+    const rectangleAreaCalculate=(parseFloat(rectangleWidthInput)*parseFloat(rectangleLengthInput)).toFixed(2)
+    const rectangleFormulaElement=document.getElementById("rectangle-formula")
+    const rectangleArea=parseFloat(rectangleFormulaElement.innerText)
+    rectangleFormulaElement.innerText=rectangleAreaCalculate
 
+    const contentTitle=document.getElementById("rectangle").innerText
+    const textAreaElement=document.getElementById("container")
+const tr=document.createElement("tr");
+tr.innerHTML=`
+<td>${serial}.</td>
+<td>${contentTitle}</td>
+<td>${rectangleAreaCalculate}cm2</td>
 
-
-
-//console.log(triangleBreadthInput)
+`
+textAreaElement.appendChild(tr)
 })
